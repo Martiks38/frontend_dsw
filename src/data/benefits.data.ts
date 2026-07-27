d949d06bca40594989c5c4f70e69a5ac71363bea
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-07-27 16:23:30 -0300

    feat: add benefits data

---
+++ src/data/benefits.data.ts
@@ -0,0 +1,26 @@
+import { HomeFeature } from '@/interfaces/homeFeature.interface';
+
+export const benefits: Array<HomeFeature> = [
+  {
+    id: 'security',
+    label: 'Seguridad',
+    description: 'Instalaciones monitoreadas 24/7 para tu tranquilidad.',
+  },
+  {
+    id: 'integral-care',
+    label: 'Cuidado integral',
+    description:
+      'Personal que cuida tu embaracación para mantenerla en excelentes condiciones.',
+  },
+  {
+    id: 'secure-storage',
+    label: 'Guardado seguro',
+    description: 'Espacios adecuados para cada tipo de embaración.',
+  },
+  {
+    id: 'on-demand-availability',
+    label: 'Cuando la necesitás',
+    description:
+      'Coordinamos el ingreso y retiro para que solo te preocupes por navegar.',
+  },
+];
