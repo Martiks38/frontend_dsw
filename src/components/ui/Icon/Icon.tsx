48e27d5fb5e9d534caa5d8df064018c9cd3ca7a9
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-07-27 12:53:12 -0300

    feat(icons): create Icon and IconSprite components

---
+++ src/components/ui/Icon/Icon.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+export type IconId =
+  | 'security'
+  | 'fuel'
+  | 'cleaning'
+  | 'integral-care'
+  | 'maintenance'
+  | 'secure-storage'
+  | 'on-demand-availability'
+  | 'boat-launch-and-retrieal';
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  id: IconId;
+  size?: number;
+}
+
+export default function Icon({ id, size = 24, className, ...rest }: IconProps) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      className={className}
+      aria-hidden="true"
+      {...rest}
+    >
+      <use href={`#${id}`} />
+    </svg>
+  );
+}
