1c67a7dd323448684d6b04b07aa28aba957d1a99
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-02 18:07:58 -0300

    refactor(HomePage): update structure and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link';
-
 import Footer from '@/components/layout/Footer/Footer';
 import Header from '@/components/layout/Header/Header';
-import HomeSection from '@/components/layout/HomeSection/HomeSection';
+import { HomeSection, IntroSection } from '@/components/layout/Section';
 import ButtonLink from '@/components/ui/Button/Button';
 import { benefits, footerColumns, services } from '@/data';
 
@@ -10,43 +8,50 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className="flex flex-col grow mx-auto px-4 w-full max-w-7xl justify-center justify-items-center">
+    <div className="mx-auto flex w-full max-w-screen-2xl grow flex-col justify-center justify-items-center">
       <Header />
-      <main>
-        <section
-          className={`${styles.intro} relative flex flex-col justify-end min-h-[70dvh] px-16 pb-20 bg-[url('/portada.webp')] bg-cover bg-position[0% 0%]`}
-        >
-          <div>
-            <h1 className="text-4xl font-semibold mb-4">
+      <main className="mt-(--height-header)">
+        <IntroSection
+          heroImageUrl="/portada.webp"
+          title={
+            <>
               Tu embarcación.
               <br />
               Nuestro cuidado.
-            </h1>
-            <p className="w-[35ch] text-2xl">
-              Guardería náutica y servicios para que disfrutes de tu embarcación
-              cuando quieras.
-            </p>
-            <nav
-              className="*:first:mr-6 mt-10 text-xl font-semibold"
-              aria-label="Servicios y solicitudes"
-            >
-              <ButtonLink href={''}>Conocé nuestros servicios</ButtonLink>
-              <ButtonLink href="" variant="secondary">
-                Solicitar guardado
-              </ButtonLink>
-            </nav>
-          </div>
-        </section>
+            </>
+          }
+        >
+          <nav
+            className="mt-10 text-xl font-semibold *:first:mr-6"
+            aria-label="Servicios y solicitudes"
+          >
+            <ButtonLink href={''}>Conocé nuestros servicios</ButtonLink>
+            <ButtonLink href="" variant="secondary">
+              Solicitar guardado
+            </ButtonLink>
+          </nav>
+        </IntroSection>
         <HomeSection title="¿Por qué elegirnos?" cardData={benefits} />
         <HomeSection
           title="Nuestros servicios"
           cardData={services.filter((s) => s.showOnHome)}
         >
-          <Link href={''}>Ver todos los servicios</Link>
+          <ButtonLink href={''} className="mx-auto mt-8 block w-fit">
+            Ver todos los servicios
+          </ButtonLink>
         </HomeSection>
-        <HomeSection title="¿Querés guardar tu embarcación con nosotros?">
+        <HomeSection
+          title="¿Querés guardar tu embarcación con nosotros?"
+          className={styles.banner}
+        >
           <p>Consultá disponibilidad y conocé nuestras condiciones.</p>
-          <Link href={''}>Solicitar guardado</Link>
+          <ButtonLink
+            href={''}
+            variant="secondary"
+            className="text-semibold text-2xl"
+          >
+            Solicitar guardado
+          </ButtonLink>
         </HomeSection>
       </main>
       <Footer
