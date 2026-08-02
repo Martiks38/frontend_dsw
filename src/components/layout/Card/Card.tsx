26e277b2aaed3a53275593613666079eac870e48
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-01 23:17:18 -0300

    styles(Card): add styles

--- src/components/layout/Card/Card.tsx
+++ src/components/layout/Card/Card.tsx
@@ -3,9 +3,9 @@ import type { HomeFeature } from '@/interfaces/homeFeature.interface';
 
 export default function Card({ data }: { data: HomeFeature }) {
   return (
-    <article>
-      <Icon id={data.id} size={60} />
-      <h3>{data.label}</h3>
+    <article className="h-full text-center">
+      <Icon id={data.id} size={60} className="mx-auto" />
+      <h3 className="mt-2.5 mb-4 text-xl font-semibold">{data.label}</h3>
       <p>{data.description}</p>
     </article>
   );
