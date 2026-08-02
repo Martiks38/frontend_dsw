ede9bde17a035436aa4289fb077e4dbe6c854154
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-02 18:05:15 -0300

    feat(componentes): create IntroSection and move HomeSection to Section folder

--- src/components/layout/Section/HomeSection/HomeSection.tsx
+++ src/components/layout/Section/HomeSection/HomeSection.tsx
@@ -1,13 +1,13 @@
 import { HomeFeature } from '@/interfaces/homeFeature.interface';
 
-import Card from '../Card/Card';
+import Card from '../../Card/Card';
 
 interface HomeSectionProps extends React.HTMLAttributes<HTMLElement> {
   title: string;
   cardData?: HomeFeature[];
 }
 
-export default function HomeSection({
+export function HomeSection({
   title,
   cardData,
   children,
@@ -15,11 +15,14 @@ export default function HomeSection({
 }: HomeSectionProps) {
   return (
     <section className={`${className ?? ''} mt-12`} aria-label={title}>
-      <h2 className="text-3xl text-center font-semibold">{title}</h2>
+      <h2 className="text-center text-3xl font-semibold">{title}</h2>
       {cardData && (
-        <ul className="flex gap-10 mt-8">
+        <ul className="mt-8 flex justify-around gap-4">
           {cardData.map((feature) => (
-            <li className="flex-1" key={feature.label}>
+            <li
+              className="max-w-60 flex-1 justify-items-center"
+              key={feature.label}
+            >
               <Card data={feature} />
             </li>
           ))}
