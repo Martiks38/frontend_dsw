a0f2d6a6c9b11ab9f780becb66f3fb0f07b8e352
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-01 23:15:15 -0300

    refactor(Header): update structure and apply new styles

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,26 +1,37 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import ButtonLink from '@/components/ui/Button/Button';
 import { navigationItems } from '@/data';
+
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className="h-14 px-8 flex justify-between items-center bg-background] text-primary font-semibold">
       <Link href={'/'}>
-        <Image src="/logo.png" width={32} height={32} alt="Guardería náutica" />
+        <Image
+          src="/logo_2.png"
+          width={150}
+          height={32}
+          alt="Guardería náutica"
+          className="h-8 w-auto"
+        />
       </Link>
       <nav aria-label="Navegación principal">
-        <ul>
+        <ul className="flex justify-between gap-x-1">
           {navigationItems.map(({ href, label }) => {
             return (
               <li key={label}>
-                <a href={href}>{label}</a>
+                <Link className={`px-2 py-1 ${styles.link}`} href={href}>
+                  <span>{label}</span>
+                </Link>
               </li>
             );
           })}
         </ul>
       </nav>
-      <a href="/login">Iniciar sesión</a>
+      <ButtonLink href="">Iniciar sesión</ButtonLink>
     </header>
   );
 }
