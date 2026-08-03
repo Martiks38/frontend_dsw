ff1aa19b272e14437e8785098171e4cac46d7622
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-02 23:55:57 -0300

    feat(Button): add animations to Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -7,8 +7,8 @@ interface ButtonLinkProps extends React.ComponentPropsWithoutRef<typeof Link> {
 }
 
 const buttonLinkVariants: Record<ButtonLinkVariants, string> = {
-  primary: 'bg-primary text-surface',
-  secondary: 'bg-surface text-primary',
+  primary: 'bg-primary text-surface hover:bg-[#114062] active:bg-[#114062]',
+  secondary: 'bg-surface text-primary hover:bg-[#f0f5fa] active:bg-[#dbe7f0]',
 };
 
 export default function ButtonLink({
@@ -21,7 +21,7 @@ export default function ButtonLink({
 
   return (
     <Link
-      className={`${variantStyles} ${className ?? ''} rounded-md border-2 px-2 py-1.5`}
+      className={`${variantStyles} ${className ?? ''} focus-visibile:outline-none focus-visible:ring-primary rounded-md border-2 px-2 py-1.5 font-medium transition-colors duration-200 focus-visible:ring-2 focus-visible:ring-offset-2`}
       href={href}
     >
       {children}
