d949f6f772d24defa8cccb949e5816ba4d94f4eb
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-11 18:40:11 -0300

    feat(StepsList): create StepsList component

---
+++ src/components/ui/Lists/StepsList/StepsList.tsx
@@ -0,0 +1,20 @@
+import { type ContractProcessStep } from '@/data/contractProcess.data';
+
+import styles from './stepsList.module.css';
+
+export default function StepsList({ steps }: { steps: ContractProcessStep[] }) {
+  return (
+    <ol className={`${styles.steps} font-bold`}>
+      {steps.map(({ step, description }) => {
+        return (
+          <li key={step}>
+            <div className={styles['step-circle']}>
+              <span className={styles['step-number']}>{step}</span>
+            </div>
+            <p className={styles['step-title']}>{description}</p>
+          </li>
+        );
+      })}
+    </ol>
+  );
+}
