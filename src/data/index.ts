0bec5a36221e81f1bef8a429c1ee3a18e6940468
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-11 18:32:56 -0300

    refactor(exports): update barrel file

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,6 @@
 export * from './benefits.data';
+export * from './contractProcess.data';
 export * from './footer.data';
+export * from './marinaImages.data';
 export * from './navigation.data';
 export * from './services.data';
