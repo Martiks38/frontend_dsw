cd54f70c64ccae985eef0cb395ebb1d037c01018
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-12 20:25:24 -0300

    refactor(Header): restructure component and update styles

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -13,31 +13,33 @@ interface HeaderProps extends React.HTMLAttributes<HTMLElement> {
 
 export default function Header({ altLogo, srcLogo }: HeaderProps) {
   return (
-    <header className="border-primary bg-background text-primary fixed top-0 left-0 z-40 flex h-(--height-header) w-full items-center justify-between border-b-2 px-8 font-semibold">
-      <Link href={'/'}>
-        <Image
-          src={srcLogo}
-          width={150}
-          height={32}
-          alt={altLogo}
-          className="h-8 w-auto"
-          priority
-        />
-      </Link>
-      <nav aria-label="Navegación principal">
-        <ul className="flex justify-between gap-x-1">
-          {NAVIGATION_ITEMS.map(({ href, label }) => {
-            return (
-              <li key={label}>
-                <Link className={`px-2 py-1 ${styles.link}`} href={href}>
-                  <span>{label}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-      <ButtonLink href="">Iniciar sesión</ButtonLink>
+    <header className="border-primary bg-background text-primary fixed top-0 left-0 z-40 h-(--height-header) w-full border-b-2 font-semibold">
+      <div className="mx-auto flex h-full max-w-screen-2xl items-center justify-between px-16">
+        <Link href={'/'}>
+          <Image
+            src={srcLogo}
+            width={150}
+            height={32}
+            alt={altLogo}
+            className="h-8 w-auto"
+            priority
+          />
+        </Link>
+        <nav aria-label="Navegación principal">
+          <ul className="flex justify-between gap-x-1">
+            {NAVIGATION_ITEMS.map(({ href, label }) => {
+              return (
+                <li key={label}>
+                  <Link className={`px-2 py-1 ${styles.link}`} href={href}>
+                    <span>{label}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <ButtonLink href="">Iniciar sesión</ButtonLink>
+      </div>
     </header>
   );
 }
