50277320e313fab95d3b05281831319682f668c3
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-13 19:18:34 -0300

    feat(IconSprite): add sprites new to svg template

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -10,7 +10,12 @@ export type IconId =
   | 'on-demand-availability'
   | 'boat-launch-and-retrieval'
   | 'battery-charging'
-  | 'repairs';
+  | 'repairs'
+  | 'certificate'
+  | 'communication'
+  | 'anchor'
+  | 'handshake'
+  | 'compass';
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {
   id: IconId;
