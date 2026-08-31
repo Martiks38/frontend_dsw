8a616bb31f576195cc6784fae160ab71bc6167f5
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-31 14:24:08 -0300

    feat(page): implement Sign In page

---
+++ src/app/(auth)/iniciar-sesion/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from '@/components/layout/SignInForm/SignInForm';
+
+export default function SignInRoute() {
+  return (
+    <div className="xs:p-6 flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <SignInForm />
+    </div>
+  );
+}
